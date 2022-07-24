ef8989a8c00b205bd5af40f0061b60868ba77a18
kikigigi <momokocute1@gmail.com>
2022-07-24 19:34:11 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Meow Meow, I want fish!
         </p>
         <a
           className="App-link"
